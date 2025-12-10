ea450a326d47ae9a9085e9ef7efa4cc5d9e0c2e8
sunny <goodluck14115@gmail.com>
2025-12-11 00:06:27 +0530

    Make Neovim dashboard beautiful

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,72 @@
+return {
+  "goolord/alpha-nvim",
+  event = "VimEnter",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
+
+    -- Set header
+
+    -- dashboard.section.header.val = {
+    --   "                                                     ",
+    --   "                                                     ",
+    --   "                                                     ",
+    --   "   ⚡ Welcome to Neovim ⚡                           ",
+    --   "                                                     ",
+    --   "                                                     ",
+    -- }
+
+    dashboard.section.header.val = {
+      "                                                     ",
+      "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+      "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+      "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+      "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+      "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+      "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+      "                                                     ",
+    }
+
+    -- Set menu
+    dashboard.section.buttons.val = {
+      dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+      dashboard.button("r", "  Recent files", ":Telescope oldfiles <CR>"),
+      dashboard.button("g", "  Find text", ":Telescope live_grep <CR>"),
+      dashboard.button("c", "  Config", ":e $MYVIMRC <CR>"),
+      dashboard.button("l", "󰒲  Lazy", ":Lazy <CR>"),
+      dashboard.button("m", "  Mason", ":Mason <CR>"),
+      dashboard.button("q", "  Quit", ":qa <CR>"),
+    }
+
+    -- Set footer
+    local function footer()
+      local total_plugins = require("lazy").stats().count
+      local datetime = os.date(" %d-%m-%Y   %H:%M:%S")
+      local version = vim.version()
+      local nvim_version_info = "   v" .. version.major .. "." .. version.minor .. "." .. version.patch
+
+      return datetime .. "   " .. total_plugins .. " plugins" .. nvim_version_info
+    end
+
+    dashboard.section.footer.val = footer()
+
+    -- Layout
+    dashboard.config.layout = {
+      { type = "padding", val = 2 },
+      dashboard.section.header,
+      { type = "padding", val = 2 },
+      dashboard.section.buttons,
+      { type = "padding", val = 1 },
+      dashboard.section.footer,
+    }
+
+    -- Disable folding on alpha buffer
+    vim.cmd([[
+      autocmd FileType alpha setlocal nofoldenable
+    ]])
+
+    alpha.setup(dashboard.config)
+  end,
+}
